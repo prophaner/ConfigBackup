7717df88d1c1be3bb3295405f07edf3901d1cba0
Luis.Ramos <lmramos89@gmail.com>
2016-08-10 16:51:25 -0400

    Fixed many bugs, check TODO file

--- configbackup.py
+++ configbackup.py
@@ -6,43 +6,25 @@ import json
 import logging
 import shutil
 import socket
-import sys
-
-TODO = """
-    -Add Requirements file
-    -Fix README.md
-    -Fix File structure
-1- Check if TFTP Server enabled by checking port 69 | Done
-2- Add Logic to compare files and only save if different
-3- Split into more defs | DONE
-4- Create a class | DONE
-5- Add more Logging instances | Neverending :)
-5- Add startup config backup | Done
-6- Create another class for each Loop instance on def loop_data() | Done
-7- Load credentials file | DONE
-8- Send email for each backup
-9- Add a Log that checks for the last time file was backed up, to be sent in every email
-10- In exec_command() add a param that checks for the type of config to be backed up | Done
-11- What if the device is not available? | Done (handled by Paramiko exceptions
-12- Remember to enable DEBUG LOGGING
-13- Add Paramiko Exceptions
-14- Add a def to get local IP address | Done
-15- Add more parameters to the config file like IP address, and only check if parameter doesn't exist
-16- Create a CMD API
-17- Replace Class instantiation when API available
-18- Change the Try and Timeout Logic after the command is sent
-19- Add an stdout debugger
-20- Change the file_change_flag logic, only check if there's a difference in the Json file and apply it
-21- Create a class for device features: name, ip, backups, credentials, version, etc..."""
+import difflib
 
 # Logging configuration
 log_file = 'logs.log'
+logging.basicConfig(filename=log_file, level=logging.DEBUG, format='%(asctime)s %(message)s')
 log2stdout = True
 
 if log2stdout:
-    logging.basicConfig(filename=log_file, level=logging.DEBUG, format='%(asctime)s %(message)s', stream=sys.stdout)
-else:
-    logging.basicConfig(filename=log_file, level=logging.DEBUG, format='%(asctime)s %(message)s')
+    # logging.basicConfig(filename=log_file, level=logging.DEBUG, format='%(asctime)s %(message)s', stream=sys.stdout)
+    logger = logging.getLogger('spam_application')
+    logger.setLevel(logging.DEBUG)
+    # create console handler with a higher log level
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.ERROR)
+    # create formatter and add it to the handlers
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    # add the handlers to the logger
+    logger.addHandler(ch)
 
 logging.info('+' * 80)
 logging.info('Script Starting')
@@ -54,16 +36,21 @@ class ConfigBackup(object):
         self.tftp_location = "C:\TFTP-Root"
         self.file_path = "config.json"
         self.cred_path = "credentials"
-        self.devices = "Devices"
+        self.devices_folder = "Devices"
         self.exec_date = time.strftime("%Y%m%d-%H%M%S")
+
+        self.server_details = ''
+        self.server_ip = ''
+        self.server_port = ''
+        self.server_hostname = ''
+        self.devices = {}
         self.file_change_flag = 0
-        self.check_times_flag = 1
-        self.port = 69
+
         self.username = ''
         self.password = ''
-        self.server_ip = ''
 
         self.data = {}
+        self.run_script = True
 
     def load_creds(self):
         """
@@ -81,51 +68,153 @@ class ConfigBackup(object):
             logging.warning('Missing credential file')
             print 'missing cred file'
             return False
-        except:
-            logging.warning('Error while loading the credentials file')
-            print 'error while loadind cred file'
-            return False
 
     def main(self):
         """
-        Loads credentials
-        Check if JSON config file exists, then load it in data dict
-        Check if self.port in use, then continue or close and report
-        Get Local server IP address
+        -Load config file
+        -Load IP address
+        -Load Port
+        -Check Port
+        -Load Credentials
+        -If all load fine, continue
         :return: None
         """
 
-        try:
-            self.get_ip_address()
-        except:
-            logging.warning("Issues getting local IP")
-            print "Issues getting local IP"
-
-        if self.load_creds() and self.check_file(self.file_path) and self.server_ip:
-            # Must split these items to better understand issues, separate them in defs to log there
-            # then have them all in an if statement that execute code if all True
+        if self.check_file(self.file_path):
             self.file2json()
             logging.debug("Data File Loaded")
 
-            if self.check_port_in_use():
-                self.loop_data()
-                self.check_changes()
-            else:
-                logging.info("Port {} open or having issues. Check TFTP Server".format(self.port))
+        if not self.server_ip:
+            try:
+                grab_first_device_ip = self.devices[self.devices.keys()[0]]['ip']
+                self.get_next_hop(grab_first_device_ip)
+                self.data['server_details']['ip'] = self.server_ip
+                self.file_change_flag = 1
+            except:
+                logging.warning("Issues getting local IP")
+                return
+
+        if not self.server_port:
+            self.server_port = 69
+            self.data['server_details']['port'] = self.server_port
+            self.file_change_flag = 1
+
+        if not self.check_port_in_use():
+            return
+
+        if not self.load_creds():
+            return "Issues loading credentials"
+
+        self.loop_data()
+
+    def loop_data(self):
+        """
+        Read each device, execute command, generate a name, generate date
+        :return:
+        """
+
+        for device_name, device_data in self.devices.items():
+            logging.info("looping on {}".format(device_name))
+
+            self.each_file(device_name, device_data, 'running')
+            self.each_file(device_name, device_data, 'startup')
+
+        self.check_changes()
+
+    def each_file(self, device_name, device_data, config_type):
+        file_name = "{}.{}.{}".format(device_name, self.exec_date, config_type)
+        src_file_path = "{}\\{}".format(self.tftp_location, file_name)
+        device_ip = device_data.get('ip')
+        old_config_file = ''
+        new_config_file = ''
+        old_config_file_name = device_data.get('{}_backup'.format(config_type))
+        add_file_flag = 0
+        command_executed_flag = 0
+
+        logging.info("Trying to backup file {}".format(file_name))
+        if device_ip:
+            try:
+                self.exec_command(device_ip, file_name, config_type, src_file_path)
+                command_executed_flag = 1
+            except:
+                logging.info("Command execution failed")
         else:
-            logging.info('Data File unavailable')
+            logging.warning('Device {} missing IP address'.format(device_name))
+
+        if command_executed_flag:
+            # Load old file if exists
+            if old_config_file_name:
+                old_file_location = '\{}\{}\{}'.format(self.devices_folder, device_name, old_config_file_name)
+                old_config_file = self.open_file(old_file_location)
+
+            # Load new file if exists
+            if self.check_file(src_file_path):
+                logging.info("File {} has been created".format(file_name))
+                new_config_file = self.open_file(src_file_path)
+            else:
+                logging.warning("File {} was not created".format(file_name))
+
+            # Compare files, if no diff, then just delete new file. Else, cut new file, change Json and Log diff
+            diff = '\n'.join(self.compare(old_config_file, new_config_file))
+            diff_d = self.changes(diff)
+            logging.info("Comparison between {} and {}".format(file_name, old_config_file_name))
+
+            if diff_d['_'] > 0:
+                if diff_d['+'] > 0 or diff_d['-'] > 0 or diff_d['?'] > 0:
+                    # Adding changes to the INFO Log if changes
+                    logging.debug(diff)
+                    logging.info("Found changes")
+                    add_file_flag += 1
+                else:
+                    logging.info("No changes found")
+                    # Flag to break
+            else:
+                logging.info("Comparison is empty".format(file_name, old_config_file_name))
+                # Flag to break
+
+            # Mark last time script ran for every file
+            self.data['devices'][device_name]['backups']['{}_last'.format(config_type)] = self.exec_date
+
+            if add_file_flag:
+                # Update JSON file to reflect file name for last time script ran
+                self.data['devices'][device_name]['backups']['{}_name'.format(config_type)] = file_name
+
+                # Move file from Download folder to Main folder
+                self.file_mover(device_name, src_file_path)
+                self.file_change_flag = 1
+            else:
+                os.remove(src_file_path)
+
+    def compare(self, old_config_file, new_config_file):
+        d = difflib.Differ()
+        diff = d.compare(old_config_file.splitlines(), new_config_file.splitlines())
+        return '\n'.join(diff)
 
     def check_file(self, name):
-        # Add a Log here to display of loaded or not, then remove the else on main
+        # Add a Log here to display if loaded or not, then remove the else on main
         return os.path.exists(name)
 
     def file2json(self):
-        with open(self.file_path) as data_file:
-            self.data = json.load(data_file)
+        try:
+            with open(self.file_path) as data_file:
+                self.data = json.load(data_file)
+        except:
+            logging.warning("JSON ERRORS")
+
+        self.server_details = self.data.get('server_details')
+        self.server_ip = self.server_details.get('ip')
+        self.server_port = self.server_details.get('port')
+        self.server_hostname = self.server_details.get('hostname')
+        self.devices = self.data.get('devices')
+
+    def open_file(self, path):
+        with open(path) as f:
+            return f.read()
 
     def json2file(self):
         with open(self.file_path, 'w') as f:
             json.dump(self.data, f, sort_keys=True, indent=4, separators=(',', ': '))
+            logging.debug("config file modified")
 
     def exec_command(self, ip, file_name, config_type, src_file_path):
         """
@@ -171,41 +260,10 @@ class ConfigBackup(object):
                         logging.debug(screen_log)
 
         ssh.close()
+
+        # Time padding for each download
+        time.sleep(1)
         return
-
-    def loop_data(self):
-        """
-        Read each device, execute command, generate a name, generate date
-        :return:
-        """
-
-        for device_name, device_data in self.data.items():
-            logging.info("looping on {}".format(device_name))
-
-            self.each_file(device_name, device_data, 'running')
-            self.each_file(device_name, device_data, 'startup')
-
-    def each_file(self, device_name, device_data, config_type):
-
-        file_name = "{}.{}.{}".format(device_name, self.exec_date, config_type)
-        src_file_path = "{}\\{}".format(self.tftp_location, file_name)
-
-        logging.info("Trying to backup file {}".format(file_name))
-        self.exec_command(device_data['IP'], file_name, config_type, src_file_path)
-
-        if self.check_file(src_file_path):
-            logging.info("File {} has been created".format(file_name))
-
-            # Update JSON file to reflect last date per config type
-            self.data[device_name]['backups']["{}_last".format(config_type)] = self.exec_date
-            # Add Logic to compare files
-
-            # Move file from Download folder to Main folder
-            self.file_mover(device_name, src_file_path)
-            self.file_change_flag = 1
-        else:
-            logging.warning("File {} was not created".format(file_name))
-            print "File {} was not created".format(file_name)
 
     def file_mover(self, device_name, src_file_path):
         """
@@ -214,13 +272,13 @@ class ConfigBackup(object):
         3- Copy file from TFTP root to folder
         4- Remove source file once copied
         """
-        dst_folder_path = self.devices + '\\' + device_name
+        dst_folder_path = self.devices_folder + '\\' + device_name
         dst_file_path = dst_folder_path + '\\' + src_file_path.split('\\')[-1]
 
         # Create Device folder if not exist
-        if not self.check_file(self.devices):
+        if not self.check_file(self.devices_folder):
             logging.debug("Device folder does't exist")
-            os.makedirs(self.devices)
+            os.makedirs(self.devices_folder)
 
         # Create each device's folder if not exist
         if not self.check_file(dst_folder_path):
@@ -248,9 +306,9 @@ class ConfigBackup(object):
 
     def check_port_in_use(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        result = sock.connect_ex(('127.0.0.1', self.port))
+        result = sock.connect_ex(('127.0.0.1', self.server_port))
         if result:
-            logging.info("Port {} in use, hopefully by the TFTP Server ;)".format(self.port))
+            logging.info("Port {} in use, hopefully by the TFTP Server ;)".format(self.server_port))
         else:
             logging.warning("Turn the TFTP Server on!!!")
         return result
@@ -265,11 +323,38 @@ class ConfigBackup(object):
         return message
 
     def get_ip_address(self):
+        """Will only get local IP, ignores VPN"""
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.connect(("www.google.com", 80))
         self.server_ip = s.getsockname()[0]
         logging.info("Server IP: {}".format(self.server_ip))
         s.close()
+
+    def get_next_hop(self, dest_ip='4.2.2.2'):
+        logging.info("Executing traceroute.")
+        ipPattern = re.compile('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
+        try:
+            raw_output = os.popen('tracert -d -h 1 -w 1 -4 {}'.format(dest_ip)).read()
+            self.server_ip = re.findall(ipPattern, raw_output)[-1]
+        except:
+            logging.warning("Error doing a Traceroute")
+
+    def changes(self, str):
+        min_flag = 0
+        plu_flag = 0
+        que_flag = 0
+        spaces = 0
+
+        for i in str.splitlines():
+            if i[0] == '-':
+                min_flag += 1
+            elif i[0] == '+':
+                plu_flag += 1
+            elif i[0] == '?':
+                que_flag += 1
+            else:
+                spaces += 1
+        return {"-": min_flag, "+": plu_flag, "?": que_flag, "_": spaces}
 
 a = ConfigBackup()
 a.main()
